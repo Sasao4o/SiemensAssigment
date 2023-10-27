841b01053cd72941c916d93f0620b1154840021e
Sasao4o <sasa1x20001@gmail.com>
2023-10-27 21:23:09 +0200

    firstBranch

--- main.cpp
+++ main.cpp
@@ -8,11 +8,19 @@ int getSum(const std::vector<int>& vec) {
     }
     return sum;
 }
+double getAverage(const std::vector<int>& vec) {
+    if (vec.empty()) {
+        return 0.0;   
+    }
+
+    int sum = getSum(vec);
+    return static_cast<double>(sum) / vec.size();
+}
 
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = getSum(numbers);
     std::cout << "Sum: " << sum << std::endl;
-
+    std::cout << "Averaege " << getAverage(numbers) << std::endl;
     return 0;
 }
